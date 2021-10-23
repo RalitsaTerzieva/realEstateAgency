c7349cf0e8d4d678a8a718ddf2ccb0f6a9f9372c
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2021-10-23 18:59:13 +0300

    Add register controller

--- routes.js
+++ routes.js
@@ -2,15 +2,17 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('./controllers/homeController.js');
+const authController = require('./controllers/authController.js');
 //const cubController = require('./controllers/cubController.js')
 //const accessoryController = require('./controllers/accessoryController.js');
-//const authController = require('./controllers/authController.js');
+
 router.get('/', (req, res) => {
     res.render('home')
 })
 
 
 router.get(homeController);
+router.get('/register', authController);
 
 
 module.exports = router
